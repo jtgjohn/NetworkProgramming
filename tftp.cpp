6b7417c21a23a8a62e747a9eede9d45d7dc87d4d
Andrew Leaf <adleaf1@gmail.com>
2018-09-23 21:32:10 -0400

    Finna bust a yeet

--- tftp.cpp
+++ tftp.cpp
@@ -1,3 +1,27 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <sys/socket.h>
+
+extern "C" {
+	#include	"../../../ExampleCode/unpv13e/lib/unp.h"
+}
+
+int
+main(int argc, char **argv)
+{
+	int					sockfd;
+	struct sockaddr_in	servaddr, cliaddr;
+
+	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(SERV_PORT);
+
+	Bind(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	printf("%d", servaddr.sin_port);
+	printf("Ahoy there");
+
+	//dg_echo(sockfd, (SA *) &cliaddr, sizeof(cliaddr));
+}
