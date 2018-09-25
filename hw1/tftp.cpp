bfba799dc7fdfe7d18f94554c02c240011378eb1
John Gay <gayj@rpi.edu>
2018-09-25 19:54:15 -0400

    it works now

--- hw1/tftp.cpp
+++ hw1/tftp.cpp
@@ -57,7 +57,6 @@ void handle_read_request(sockaddr_in* servaddr, socklen_t sockaddr_length, const
 			done = 1;
 		}
 
-		printf("file packet: %s with %d bytes\n", packet, bytesread);
 		blocknum++;
 
 		for (; attempts < RETRIES; attempts++) {
@@ -66,20 +65,18 @@ void handle_read_request(sockaddr_in* servaddr, socklen_t sockaddr_length, const
 
 			uint16_t opcode = htons(DATA);
 			uint16_t block = htons(blocknum);
-			printf("sendop: %d to %d block: %d to %d\n",DATA, opcode, blocknum, block);
 			char data[bytesread + 4];
 			memcpy(data, (char*)&opcode, 2);
 			memcpy(data + 2, (char*)&block, 2);
 			memcpy(data + 4, packet, bytesread);
 
-			printf("Packet to send: %s\n", data);
-
+			printf("Sending data packet: block number %d with %d bytes \n",blocknum, bytesread);
 			sendto(clifd, data, 4 + bytesread, 0, (struct sockaddr *)servaddr, sockaddr_length);
 
 			/// receive response
 				//if response received, break
 			alarm(1);
-			nrec = recvfrom(clifd, m, 4, 0, (struct sockaddr *)& servaddr, (socklen_t *)sockaddr_length);
+			nrec = recvfrom(clifd, m, 4, 0, NULL, NULL);
 			if (nrec < 0) {
 				perror("receive error in RRQ");
 				return;
@@ -106,6 +103,7 @@ void handle_read_request(sockaddr_in* servaddr, socklen_t sockaddr_length, const
 				return;
 			}
 			else {
+				printf("Ack received with block nubmer %d\n", bnum );
 				break;
 			}
 
@@ -118,8 +116,9 @@ void handle_read_request(sockaddr_in* servaddr, socklen_t sockaddr_length, const
 
 	}
 
-
+	printf("Done reading from file!\n\n");
 	fclose(file);
+	return;
 }
 
 void handle_write_request(sockaddr_in* servaddr, socklen_t sockaddr_length, const char* fname) {
@@ -137,7 +136,6 @@ void handle_write_request(sockaddr_in* servaddr, socklen_t sockaddr_length, cons
 	uint16_t block = htons(blocknum);
 	memcpy(ackpack, (char*)&opcode, 2);
 	memcpy(ackpack + 2, (char*)&block, 2);
-	printf("ackpack %s\n", ackpack);
 	sendto(clifd, ackpack, 4, 0, (struct sockaddr*)servaddr, sockaddr_length);
 
 
@@ -156,64 +154,66 @@ void handle_write_request(sockaddr_in* servaddr, socklen_t sockaddr_length, cons
 		int sockfd = 0;;
 		int	n;
 		socklen_t len;
-		char mesg[PACKET_SIZE];
+		unsigned char mesg[PACKET_SIZE + 4];
 		blocknum++;
 
 		len = sizeof(servaddr);
 
 		for(; attempts < RETRIES; attempts++) {
 			// Receive data
-
-			printf("RECEIVED: ");
-			n = Recvfrom(clifd, mesg, PACKET_SIZE, 0, (SA *) &servaddr, &len);
-			//data will have opcode of 3
-			for (int i=0;i<n;i++) {
-				//printf("%d",mesg[i]);
-				messageText += mesg[i];
-				printf("%d", mesg[i]);
-				if (i == 1) {
-					opcode = mesg[i];
-				}
+			if ((n = recvfrom(clifd, mesg, PACKET_SIZE +4, 0, NULL, NULL)) < 0) {
+				printf("Error receiving data\n");
+				return;
 			}
+			uint16_t* opp = (uint16_t*)mesg;
+			opcode = ntohs(*opp);
 
-			printf("\n");
+			//data will have opcode of 3
 
-				// If received, break
-			// Send ack 
 
+			// If received, break
 			alarm(1);
 			if (opcode == DATA) {
 				break;
 			}
-			if (n < PACKET_SIZE) {
+		}
+		if (n < PACKET_SIZE) {
 				done = 1;
 			}
-		}
-
+		alarm(0);
 		if(attempts > 9) {
 			perror("Retried 10 times, no response ..... aborting\n");
 			exit(1);
 		}
+
+		printf("Data received! %d bytes\n", n-4);
+		unsigned char data[n-4];
+		memcpy(data, mesg+4, n-4);
+
+		//write data to file here
+		fwrite(data, 1, n-4, file);
+
+
 		unsigned char sendack[4];
 		block = htons(blocknum);
 		opcode = htons(ACK);
 		memcpy(sendack, (char*)&opcode, 2);
 		memcpy(sendack + 2, (char*)&block, 2);
-		printf("sendack %s\n", sendack);
+		printf("Sending acknowledgement, block number %d\n", blocknum);
 		
-		if (sendto(clifd, ackpack, 4, 0, (struct sockaddr*)servaddr, sockaddr_length) < 0) {
-			if(errno == EFAULT)
+		if (sendto(clifd, sendack, 4, 0, (struct sockaddr*)servaddr, sockaddr_length) < 0) {
 			perror("error sending ack to client\n");
 			exit(1);
 		}
 
-		//write data to file here
-		fputs(messageText.c_str(), file);
+
 
 
 	}
 
 	fclose(file);
+	printf("Done writing to file!\n\n");
+	return;
 
 }
 
@@ -266,11 +266,9 @@ int main(int argc, char **argv)
 
 
 
-	///TODO: Read client request, store opcode and filename into  variables
+	/// Read client request, store opcode and filename into  variables
 
 	//server is up, start waiting for a message
-	//dg_echo(sockfd, (SA *) &cliaddr, sizeof(cliaddr));
-	
 	int			n;
 	socklen_t	len;
 	char		mesg[MAXLINE];
@@ -280,68 +278,43 @@ int main(int argc, char **argv)
 
 		n = recvfrom(sockfd, mesg, MAXLINE, 0, (SA *) &cliaddr, &len);
 
-		//mesg[n-1] = '\0';
-		//std::string message = mesg;
-		//printf("Op code num is %s\n", message.c_str());
-
-		//std::cout << "break" << std::endl;
-
 		std::string messageText = "";
 
 		uint16_t opcode;
 		uint16_t* opp = (uint16_t*)mesg;
 		opcode = ntohs(*opp);
 
-
-
-		printf("PACKET: ");
 		for (int i=0;i<n;i++)
 		{
-			printf("%d",mesg[i]);
 			messageText += mesg[i];
-			// if (i == 1) {
-			// 	opcode = mesg[i];
-			// }
 		}
 		
-		printf("\n");
+
 		std::string fileName = messageText.substr(2, messageText.find("octet"));
 		const char* fname = fileName.c_str();
-		//std::cout << "The message text is: " + messageText << std::endl; 
-		
-		//printf("The file name is %s\n", fileName.c_str());
-		//std::cout << "The opcode is:" + int(opcode) << std::endl;
+
 		printf("OPCODE: %d\n", opcode);
 		if(opcode != RRQ && opcode != WRQ) {
 			perror("wrong\n");
 			exit(1);
-			// *opp = htons(ERROR);
-			// *(opp+1) = htons(4);
-			// *(mesg + 4) = 0;
-
-			// sendto(sockfd, mesg, 5, 0, (struct sockaddr *)&servaddr, sockaddr_length);
+	
 
 		}
 		else {
 			if (fork() == 0) {
 				printf("The fileName is: %s\n",fname);
 				if (opcode == RRQ) {
+					printf("Writing to file!\n");
 					handle_read_request(&cliaddr, len, fname);
 				}
 				else if (opcode == WRQ) {
+					printf("Reading from file!\n");
 					handle_write_request(&cliaddr, len, fname	);
 				}
 
 				exit(0);
 			}
 		}
-
-		//std::cout << "n: " + n << std::endl;
-		//std::cout << "sockfd: " + sockfd << std::endl;
-		//std::cout << "n: "<< n << std::endl;
-		//std::cout << "msg: " + std::string(mesg) << std::endl;
-		//printf("%s", mesg);
-
 
 	}
 
@@ -350,20 +323,3 @@ int main(int argc, char **argv)
 	close(sockfd);
 	return 0;
 }
-
-/*
-dg_echo(int sockfd, SA *pcliaddr, socklen_t clilen)
-{
-	int			n;
-	socklen_t	len;
-	char		mesg[MAXLINE];
-
-	for ( ; ; ) {
-		len = clilen;
-		n = Recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
-
-		Sendto(sockfd, mesg, n, 0, pcliaddr, clilen);
-	}
-}
-
-*/
