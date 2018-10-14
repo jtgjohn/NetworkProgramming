2ec1197e4ba90cfca4a4ab8652a4c744f66dfb43
Andrew <adleaf1@gmail.com>
2018-10-14 17:42:00 -0400

    Takes input from user

--- hw2/hw2server.cpp
+++ hw2/hw2server.cpp
@@ -24,8 +24,46 @@ int main(int argc, char* argv[]) {
 	}
 
 	std::cout << "END of file" << std::endl;
-	for (int i = 0; i < wordsList.size(); i++) {
-		std::cout << wordsList[i];
+	//for (int i = 0; i < wordsList.size(); i++) {
+	//	std::cout << wordsList[i];
+	//}
+
+	//int randomIndex = rand() % wordsList.size();
+
+	//randomWord = wordsList[randomIndex];
+
+	while(1) {
+		int randomIndex = rand() % wordsList.size();
+
+		std::string randomWord = wordsList[randomIndex];
+
+		std::cout << randomWord << std::endl;
+
+		//create a char vector from the chosen word
+		std::vector<char> wordInfo;
+
+		for (int i = 0; i < randomWord.size(); i++) {
+			wordInfo.push_back(randomWord[i]);
+		}
+
+		std::cout << "Enter guesses" << std::endl;
+
+		while(1) {
+			
+
+			std::string guess;
+
+			std::cin >> guess;
+
+			std::cout << "Guess " << guess << " " << guess.size() << std::endl;
+			std::cout << std::endl;
+			std::cout << "Word " << randomWord << " " << randomWord.size() << std::endl;
+
+			if (guess.size() != randomWord.size()) {
+				std::cout << "Wrong length" << std::endl;
+			}
+		}
+
 	}
 
 }
