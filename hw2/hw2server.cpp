04091755721fa7c3d6d4ef13b2f939658a45a73e
Andrew <adleaf1@gmail.com>
2018-10-14 04:27:20 -0400

    Read in dictionary

--- hw2/hw2server.cpp
+++ hw2/hw2server.cpp
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
 
-int main() {
+int main(int argc, char* argv[]) {
 
 	printf("Test\n");
+
+	std::string dictionary = argv[1];
+	std::vector<std::string> wordsList;
+
+	std::ifstream input(dictionary.c_str());
+
+	if (!input) {
+		std::cout << "Cannot open input file" << std::endl;
+	}
+
+	for (std::string line; getline(input, line);) {
+		wordsList.push_back(line);
+		//std::cout << line << std::endl;
+	}
+
+	std::cout << "END of file" << std::endl;
+	for (int i = 0; i < wordsList.size(); i++) {
+		std::cout << wordsList[i];
+	}
 
 }
